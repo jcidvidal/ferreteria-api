a6b1d4d6e60008ea1f3d83b88ea78228f2a3a447
JorgePfeifer <j.pfeifer01@ufromail.cl>
2025-05-22 16:50:36 -0400

    Cambios de frontend admin
    Se hizo el frond  de admin segun los nuevos cambios de reactjs

---
+++ src/main/resources/static/ferreteria-frontend/src-admin/ReclamosSugerencias/ReclamosSugerencias.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ReclamosSugerencias.module.css';
+
+const ReclamosSugerencias = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Reclamos y Sugerencias</h1>
+    </div>
+  );
+};
+
+export default ReclamosSugerencias;
