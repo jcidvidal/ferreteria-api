914b021fe0610bdd3be2d58757c936ffba65c70d
m4c4c0-spec <m.venegas14@ufromail.cl>
2025-06-11 19:01:32 -0400

    Arreglo NavBar para los archivos

--- src/main/resources/static/ferreteria-frontend/admin/src/App.js
+++ src/main/resources/static/ferreteria-frontend/admin/src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Inicio from './Inicio/Inicio.js';
 import Clientes from './Clientes/Clientes.js';
 import Ventas from './Ventas/Ventas.js';
 import Stock from './Stock/Stock.js';
-import ReclamosSugerencias from './ReclamosSugerencias/ReclamosSugerencias.js';
+import ReclamosSugerencias from './ReclamosSugerencias/Reclamos.js';
+import Login from './Login/Login.js';
 
 function App() {
   return (
     <Router>
-      <nav style={{ background: '#f5f5f5', padding: 10, marginBottom: 20 }}>
-        <Link to="/" style={{ margin: 8 }}>Inicio</Link>
-        <Link to="/clientes" style={{ margin: 8 }}>Clientes</Link>
-        <Link to="/ventas" style={{ margin: 8 }}>Ventas</Link>
-        <Link to="/stock" style={{ margin: 8 }}>Stock</Link>
-        <Link to="/reclamos" style={{ margin: 8 }}>Reclamos y Sugerencias</Link>
-      </nav>
       <Routes>
-        <Route path="/" element={<Inicio />} />
+        <Route path="/Inicio" element={<Inicio />} />
+        <Route path="/" element={<Login />} />
         <Route path="/clientes" element={<Clientes />} />
         <Route path="/ventas" element={<Ventas />} />
         <Route path="/stock" element={<Stock />} />
