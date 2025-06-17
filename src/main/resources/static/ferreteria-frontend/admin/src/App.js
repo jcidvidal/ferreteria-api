b7f65f197fd8d5dc5e4605444b61c66508358137
m4c4c0-spec <m.venegas14@ufromail.cl>
2025-06-17 18:54:56 -0400

    se elimina navbar que estaba incorrecta

--- src/main/resources/static/ferreteria-frontend/admin/src/App.js
+++ src/main/resources/static/ferreteria-frontend/admin/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from './inicio/Inicio.js';
 import Clientes from './clientes/Clientes.js';
 import Ventas from './ventas/Ventas.js';
@@ -9,13 +9,7 @@ import ReclamosSugerencias from './reclamos/Reclamos.js';
 function App() {
   return (
     <Router>
-      <nav style={{ background: '#f5f5f5', padding: 10, marginBottom: 20 }}>
-        <Link to="/" style={{ margin: 8 }}>Inicio</Link>
-        <Link to="/clientes" style={{ margin: 8 }}>Clientes</Link>
-        <Link to="/ventas" style={{ margin: 8 }}>Ventas</Link>
-        <Link to="/stock" style={{ margin: 8 }}>Stock</Link>
-        <Link to="/reclamos" style={{ margin: 8 }}>Reclamos y Sugerencias</Link>
-      </nav>
+    
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/clientes" element={<Clientes />} />
